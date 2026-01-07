b01f35c78ef141ebeca09a7ace47771c2f3c3fde
Trung Nghi <nghichau2k5@gmail.com>
2026-01-07 18:38:42 +0700

    ahihi

--- 113.py
+++ 113.py
@@ -1,8 +1,5 @@
 print("demo 1234")
 print("hello ae")
-<<<<<<< HEAD
 print("Hope fix")
 print("sưa fix")
-=======
-print("Tao conflict")
->>>>>>> 5d76cc642815c5f81eac9868156e7614cbcd25e2
+#fsdaf csa
